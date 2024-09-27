ceceb114c123a7e4f9d6988708f917a0670e49d8
Migueldsc12 <19-10274@usb.ve>
2024-09-27 15:20:49 -0400

    fix test problems

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,15 @@
 // Endpoint for querying the fibonacci numbers
 import {Request, Response} from 'express';
-import { stringify } from 'ts-jest';
 import { fibonacci } from './fib';
 
 export default (req: Request, res: Response) => {
   const { num }  = req.params;
 
   const fibN: number = fibonacci(parseInt(num));
-  let result = "fibonacci(" + stringify(num) + ") is " + stringify(fibN);
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
-    result = "fibonacci(" + stringify(num) + ") is undefined";
+    result = `fibonacci(${num}) is undefined`;
   }
 
   res.send(result);
